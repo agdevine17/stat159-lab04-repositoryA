dae678f82a9c582310417aaa3578531e0be46003
Ubuntu <agdevine17@berkeley.edu>
2026-09-22 16:59:18 -0700

    set up generate data script

---
+++ generate-data.R
@@ -0,0 +1,9 @@
+library(tidyverse)
+
+set.seed(42)
+df <- tibble(
+  Transaction_ID = 1001:1100,
+  Store_ID = sample(c("Store_A", "Store_B", "Store_C", NA), 100, replace = TRUE, prob = c(0.4, 0.3, 0.2, 0.1)),
+  Revenue = sample(c(50, 120, 250, 500, NA), 100, replace = TRUE, prob = c(0.3, 0.4, 0.15, 0.10, 0.05)),
+  Units_Sold = sample(1:12, 100, replace = TRUE)
+)
