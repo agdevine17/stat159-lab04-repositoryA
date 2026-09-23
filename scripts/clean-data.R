a982ae97ccc0c7e6ff7aed5ac0d6a6a1de2a34a7
Ubuntu <agdevine17@berkeley.edu>
2026-09-22 17:24:08 -0700

    edit clean-data script to get cleaned data set

--- scripts/clean-data.R
+++ scripts/clean-data.R
@@ -1,6 +1,33 @@
-raw_df <- read.csv(data/transavtions.csv)
+library(tidyverse)
+raw_df <- read.csv("data/transactions.csv")
 
 raw_df %>%
   summarise(across(everything(), ~ sum(is.na(.))))
-raw_df <- raw_df %>%
-  drop_na(Store_ID)
+
+clean_df <- raw_df %>%
+ drop_na(Store_ID)
+
+clean_df <- clean_df %>%
+  group_by(Units_Sold) %>%
+  mutate(
+    Revenue = if_else(
+      is.na(Revenue),
+      median(Revenue, na.rm = TRUE),
+      Revenue
+    )
+  ) %>%
+  ungroup()
+
+overall_median <- median(clean_df$Revenue, na.rm = TRUE)
+clean_df <- clean_df %>%
+  mutate(
+    Revenue = if_else(
+      is.na(Revenue),
+      overall_median,
+      Revenue
+    )
+  )
+clean_df %>%
+  summarise(across(everything(), ~ sum(is.na(.))))
+
+write_csv(clean_df, "data/transactions_clean.csv")
